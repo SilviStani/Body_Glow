bfd187d5c6f5a9946c201772887dd9ff0b3245ca
SilviStani <silvina.stani@gmail.com>
2025-12-11 15:54:58 -0300

    adding sections and information

---
+++ Components/Presentation.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Presentation.scss';
+
+const Presentation = () => {
+    return (
+        <div className="PresentationWrapper">
+            <img
+                src="/masaje.jpg"
+                alt="Presentation"
+                className="PresentationImage"
+            />
+        </div>
+    );
+};
+
+export default Presentation;
--- src/app/page.js
+++ src/app/page.js
@@ -1,49 +1,16 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.scss";
+import NavBar from "../../Components/NavBar";
+import Presentation from "../../Components/Presentation";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>Y aqui comienza TODO!</h1>
-          <p>
-            Pagina de Body Glow en Construccionx  
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Paciencia
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Y Mucho Amor
-          </a>
-        </div>
+    <div className="page">
+      <main className="main">
+        <NavBar />
+        <Presentation />
+        <Presentation />
+        <Presentation />
+        <Presentation />
       </main>
     </div>
   );
